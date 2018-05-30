0db3e54d50557348dcce7d066791e6aca6d2c9be
Maximilian Meister <mmeister@suse.de>
2018-05-30 10:03:54 +0200

    link not clickable when selecting the parent via xpath
    we only need the parent for the "data-id"
    
    Signed-off-by: Maximilian Meister <mmeister@suse.de>
    (cherry picked from commit f4911052f76f5b99116b1c6d542cec165a6361ea)

--- velum-bootstrap/spec/features/07-node-remove.rb
+++ velum-bootstrap/spec/features/07-node-remove.rb
@@ -34,13 +34,13 @@ feature "Remove a Node" do
 
     puts ">>> Click to remove a node"
     with_screenshot(name: :node_removal) do
-      node_link = find(".remove-node-link", match: :first).first(:xpath,".//..")
       # mark node as inactive in environment.json
+      node_to_remove = find(".remove-node-link", match: :first)
       environment(
         action: :update,
-        body: set_minion_status(node_link["data-id"], "removed")
+        body:   set_minion_status(node_to_remove.first(:xpath, ".//..")["data-id"], "removed")
       )
-      node_link.click
+      node_to_remove.click
     end
 
     if page.has_content?("Unsupported cluster topology", wait: 5)
@@ -69,11 +69,7 @@ feature "Remove a Node" do
     puts ">>> Checking if node removal orchestration succeeded"
     with_screenshot(name: :node_removal_orchestration_succeeded) do
       within(".nodes-container") do
-<<<<<<< HEAD
-        expect(page).to have_css(".fa-check-circle-o", count: node_number-1, wait: 5)
-=======
         expect(page).to have_css(".fa-check-circle-o", count: node_number, wait: 5)
->>>>>>> 0692071... make node_number global to avoid DRY
       end
       expect(page).not_to have_text("Removal Failed", wait: 5)
     end
