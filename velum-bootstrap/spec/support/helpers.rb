3a1afa4127ff2caa303057817e63ab3a92aa7a90
Vítor Avelino <vavelino@suse.com>
2019-01-28 18:31:43 -0300

    velum-bootstrap: check content disposition instead of url
    The spec for checking the kubeconfig download was previously done
    through checking an url associated with the automatic download.
    
    A better way of checking the automatic download is to check the
    content disposition in the response header.
    
    The previous behavior to manually download kubeconfig was maintained in
    order to use the kubeconfig in further tests. The only difference is
    that is compatible with both GET and POST requests due to different
    versions in different repos. In the future we can remove the GET and
    keep only the POST one.

--- velum-bootstrap/spec/support/helpers.rb
+++ velum-bootstrap/spec/support/helpers.rb
@@ -26,6 +26,31 @@ module Helpers
     puts "<<< User logged in"
   end
 
+  def download_kubeconfig
+    # TODO: remove in the future when POST gets deployed
+    matches = page.html.match(/window\.location\.href = "(.*?)"/)
+
+    download_uri = if matches.nil?
+      URI(page.html.match(/action="(.*?)"/).captures[0])
+    else
+      URI(matches.captures[0])
+    end
+
+    http = Net::HTTP.new(download_uri.host, download_uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = if page.has_css?("form")
+      Net::HTTP::Post.new(download_uri.request_uri)
+    else
+      # TODO: remove in the future when POST gets deployed
+      Net::HTTP::Get.new(download_uri.request_uri)
+    end
+    response = http.request(request)
+
+    File.write("kubeconfig", response.body)
+  end
+
   def default_interface
     `awk '$2 == 00000000 { print $1 }' /proc/net/route`.strip
   end
@@ -33,7 +58,6 @@ module Helpers
   def default_ip_address
     `ip addr show #{default_interface} | awk '$1 == "inet" {print $2}' | cut -f1 -d/`.strip
   end
-
 end
 
 RSpec.configure { |config| config.include Helpers, type: :feature }
