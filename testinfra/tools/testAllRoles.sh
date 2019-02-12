098e8f8499be91f071d0041008d348fc94f261ec
Ludovic Cavajani <lcavajani@suse.com>
2019-02-12 14:10:07 +0100

    remove unused variable
    FILE was used and ENVIRONMENT_JSON was not used, in order
    to keep consistent with the other script, FILE is removed
    in favor of ENVIRONMENT (previously ENVIRONMENT_JSON)
    
    Signed-off-by: Ludovic Cavajani <lcavajani@suse.com>

--- testinfra/tools/testAllRoles.sh
+++ testinfra/tools/testAllRoles.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 set -eu
 
-FILE=$1
+ENVIRONMENT=$1
 SSH_CONFIG=$2
 POSARGS=${3:-}
-ENVIRONMENT_JSON=$FILE
 
 for role in "worker" "master" "admin"; do
-    fqdns=$(jq -r "[.minions[] | select(.role==\"$role\") | .fqdn ] | join(\",\" )" $FILE)
+    fqdns=$(jq -r "[.minions[] | select(.role==\"$role\") | .fqdn ] | join(\",\" )" $ENVIRONMENT)
     if [ -n "$fqdns" ]; then
         for fqdn in ${fqdns//,/ }; do
-            status=$(jq -r "[.minions[] | select(.fqdn==\"$fqdn\") | .status ] | join(\",\" )" $FILE)
+            status=$(jq -r "[.minions[] | select(.fqdn==\"$fqdn\") | .status ] | join(\",\" )" $ENVIRONMENT)
             pytest --ssh-config=$SSH_CONFIG --connection ssh --sudo -m "($role or common) and $status" --hosts $fqdn --junit-xml ${fqdn}.xml -v $POSARGS
         done
     fi
